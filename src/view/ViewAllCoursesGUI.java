74f05d8f16306d82c4e5d5e0de270ce6b7890e8c
trapwaifu <117154224+trapwaifu@users.noreply.github.com>
2022-11-08 18:44:32 +0900

    Added View class for Table GUI

---
+++ src/view/ViewAllCoursesGUI.java
@@ -0,0 +1,12 @@
+package view;
+
+public class ViewAllCoursesGUI implements View {
+	ViewAllCoursesGUI() {
+		
+	}
+	private static final String[] columnNames =  {"코드", "과목명", "분반", "교수님", "대상", "학점", "비고", "정원", "설계", "개설학부"};
+	@Override
+	public void view() {
+		
+	}
+}
