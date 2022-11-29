20485715603001538477cfb03c0d1ab99edd4e5e
trapwaifu <117154224+trapwaifu@users.noreply.github.com>
2022-11-29 14:56:28 +0900

    integrated image save module in tmp

--- src/event/ClickSaveImage.java
+++ src/event/ClickSaveImage.java
@@ -1,18 +1,21 @@
 package event;
 
+import java.awt.FileDialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 
 public class ClickSaveImage implements ActionListener {
 	JLayeredPane pane;
-		
+
 	public ClickSaveImage(JLayeredPane pane) {
 		super();
 		this.pane = pane;
@@ -20,18 +23,32 @@ public class ClickSaveImage implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		try {
-			BufferedImage im = new BufferedImage(
-					pane.getWidth(), pane.getHeight(), BufferedImage.TYPE_INT_ARGB);
-			pane.paint(im.getGraphics());
-			ImageIO.write(im, "PNG", new File("shot.png"));
-			JOptionPane.showMessageDialog(null, "이미지 저장 완료");
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			System.out.println("cannot save image");
-		}
+			//			BufferedImage im = new BufferedImage(
+			//					pane.getWidth(), pane.getHeight(), BufferedImage.TYPE_INT_ARGB);
+			//			pane.paint(im.getGraphics());
+			//			ImageIO.write(im, "PNG", new File("shot.png"));
+			//			JOptionPane.showMessageDialog(null, "이미지 저장 완료");
+
+			FileDialog fd = new FileDialog(new JFrame(), "save", FileDialog.SAVE);
+			fd.setVisible(true);
+
+			String path = fd.getDirectory() + fd.getFile();
+			if(!path.endsWith(".png"))
+				path = path + ".png";
+
+			try {
+				BufferedImage im = new BufferedImage(
+						pane.getWidth(), pane.getHeight(), BufferedImage.TYPE_INT_ARGB);
+				pane.paint(im.getGraphics());
+				ImageIO.write(im, "PNG", new File("shot.png"));
+
+			} 
+			catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				System.out.println("cannot save image");
+			}
+
+
 	}
-	
-	
 }
